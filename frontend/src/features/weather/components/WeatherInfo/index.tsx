032f02aadc6d3f50c5c3a1db59656656b8b9cfad
Arkadiusz Sapek <sapek.arkadiusz@gmail.com>
2020-08-30 16:52:25 +0200

    Connect general weather info component to store

--- frontend/src/features/weather/components/WeatherInfo/index.tsx
+++ frontend/src/features/weather/components/WeatherInfo/index.tsx
@@ -1,39 +1,66 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { fontSize } from 'config/variablesConfig';
 
 import { DaytimeWeather } from '../../models/weatherModel';
-import { WeatherParam } from '../WeatherParam';
 import { WeatherLineChart } from '../../components/WeatherLineChart';
+import {
+    getWeatherForSelectedDate,
+    getWeatherPlaceNameSelector,
+} from '../../selectors/weatherSelectors';
+import { WeatherParam } from '../WeatherParam';
 import * as S from './styles';
 
 interface Props {
     weather: DaytimeWeather;
 }
 
-export const WeatherInfoComponent: React.FC<Props> = ({ weather }) => (
-    <S.Wrapper>
-        {/* <S.PlaceHeader>{weather.placeName}</S.PlaceHeader> */}
-        <S.WeatherDate>{weather.applicableDate.toDateString()}</S.WeatherDate>
-        <S.GeneralWeatherInfoWrapper>
-            <WeatherParam
-                label="MAX"
-                valueFontSize={fontSize.large}
-                value={`${Math.round(weather.maxTemp)}°`}
-            />
-            <S.WeatherStateNameWrapper>{weather.stateName}</S.WeatherStateNameWrapper>
-        </S.GeneralWeatherInfoWrapper>
+export const WeatherInfoComponent: React.FC<Props> = () => {
+    const daytimeWeather = useSelector(getWeatherForSelectedDate);
+    const placeName = useSelector(getWeatherPlaceNameSelector);
 
-        <S.WeatherParamsWrapper>
-            <WeatherParam label="MAX" value={`${Math.round(weather.maxTemp)}°`} />
-            <WeatherParam label="MIN" value={`${Math.round(weather.minTemp)}°`} />
-            <WeatherParam label="HUMIDITY" value={Math.round(weather.humidity)} subscript="%" />
-            <WeatherParam
-                label="WIND SPEED"
-                value={Math.round(weather.humidity)}
-                subscript="km/h"
-            />
-        </S.WeatherParamsWrapper>
-        <WeatherLineChart />
-    </S.Wrapper>
-);
+    return (
+        <S.Wrapper>
+            {daytimeWeather && (
+                <>
+                    <S.PlaceHeader>{placeName}</S.PlaceHeader>
+
+                    <S.WeatherDate>{daytimeWeather.applicableDate.toDateString()}</S.WeatherDate>
+                    <S.GeneralWeatherInfoWrapper>
+                        <WeatherParam
+                            label="MAX"
+                            valueFontSize={fontSize.large}
+                            value={`${Math.round(daytimeWeather.maxTemp)}°`}
+                        />
+                        <S.WeatherStateNameWrapper>
+                            {daytimeWeather.stateName}
+                        </S.WeatherStateNameWrapper>
+                    </S.GeneralWeatherInfoWrapper>
+
+                    <S.WeatherParamsWrapper>
+                        <WeatherParam
+                            label="MAX"
+                            value={`${Math.round(daytimeWeather.maxTemp)}°`}
+                        />
+                        <WeatherParam
+                            label="MIN"
+                            value={`${Math.round(daytimeWeather.minTemp)}°`}
+                        />
+                        <WeatherParam
+                            label="HUMIDITY"
+                            value={Math.round(daytimeWeather.humidity)}
+                            subscript="%"
+                        />
+                        <WeatherParam
+                            label="WIND SPEED"
+                            value={Math.round(daytimeWeather.humidity)}
+                            subscript="km/h"
+                        />
+                    </S.WeatherParamsWrapper>
+                    <WeatherLineChart />
+                </>
+            )}
+        </S.Wrapper>
+    );
+};
---
+++ frontend/src/features/weather/helpers/checkIfDatesAreOnSameDay.ts
@@ -0,0 +1,4 @@
+export const checkIfDatesAreOnSameDay = (first: Date, second: Date) =>
+    first.getFullYear() === second.getFullYear() &&
+    first.getMonth() === second.getMonth() &&
+    first.getDate() === second.getDate();
